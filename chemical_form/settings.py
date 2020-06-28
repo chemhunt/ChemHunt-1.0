c3e63074906e86907b60cd19d3fb4a8ce63ade25
mscreation <makwanashyam28@gmail.com>
2020-06-28 21:51:55 +0530

    First Commit

--- chemical_form/settings.py
+++ chemical_form/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'i%&2mze@_5xot=4di_ovdb-ygk4@m_m%z9zm58z-h1$2ny#n-h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chemicalexam.herokuapp.com']
 
 
 # Application definition
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
