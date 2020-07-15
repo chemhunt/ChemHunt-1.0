b8b98ad8d289b57adcc937a59d203f1aa581c222
unknown <makwanashyam28@gmail.com>
2020-07-15 23:57:06 +0530

    Day4

--- formapp/static/js/timer.js
+++ formapp/static/js/timer.js
@@ -1,15 +1,15 @@
 var min = 60;
 var sec = 0;
 function loadtimer() {
-    if(localStorage.getItem("min3") && localStorage.getItem("sec3")) {
-        min = localStorage.getItem("min3");
-        sec = localStorage.getItem("sec3");
+    if(localStorage.getItem("min4") && localStorage.getItem("sec4")) {
+        min = localStorage.getItem("min4");
+        sec = localStorage.getItem("sec4");
     }
     else {
         min = 60;
         sec = 0;
-        localStorage.setItem("min3", min);
-        localStorage.setItem("sec3",sec);
+        localStorage.setItem("min4", min);
+        localStorage.setItem("sec4",sec);
     }
     timer();
 }
@@ -17,13 +17,13 @@ function timer() {
 
     if(sec!=0) {
         sec--;
-        localStorage.setItem("sec3",sec);
+        localStorage.setItem("sec4",sec);
     }
     else {
         sec=59;
         min--;  
-        localStorage.setItem("min3", min);  
-        localStorage.setItem("sec3",sec);
+        localStorage.setItem("min4", min);  
+        localStorage.setItem("sec4",sec);
     }
     document.getElementById("time").innerHTML = min + ":" + (sec<10?("0"+sec):sec);
     if(sec==0 && min==0) {
